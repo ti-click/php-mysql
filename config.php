e121ada8adce0396fccadf7e20cbe02768ff1d22
Chen Cheng <2803470+it2911@users.noreply.github.com>
2021-12-25 20:03:50 +0800

    Update config.php
    Update TiDB info

--- config.php
+++ config.php
@@ -7,8 +7,8 @@
 
 $host       = "localhost";
 $username   = "root";
-$password   = "root";
-$dbname     = "test";
+$password   = "";
+$dbname     = "users";
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
